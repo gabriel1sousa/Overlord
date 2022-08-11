1cf35d11720c13506db4d334c78c870346c6a8ae
gabriel1sousa <105893429+gabriel1sousa@users.noreply.github.com>
2022-08-11 14:40:20 -0300

    Add files via upload
    atividade módulo 7

---
+++ Aluno.java
@@ -0,0 +1,20 @@
+public class Aluno {
+
+    //características
+    String nome;
+    String anoletivo;
+    String curso;
+    int idade;
+
+    //métodos
+       void assistirAula(){
+
+       }
+       void fazerAtividade(){
+
+       }
+       void calculoNota(){
+
+       }
+
+    }
