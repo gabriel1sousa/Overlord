85d8818d470929bb55d6334563310461471d2ec6
gabriel1sousa <105893429+gabriel1sousa@users.noreply.github.com>
2022-08-12 15:46:21 -0300

    Add files via upload
    Atividade módulo 8.

---
+++ tarefacalculomedia.java
@@ -0,0 +1,17 @@
+public class tarefacalculomedia {
+
+    public static void main(String args[]) {
+
+        int nota1 = 10;
+        int nota2 = 9;
+        int nota3 = 6;
+        int nota4 = 7;
+        int divisor = 4;
+
+        int media= (nota1 + nota2 + nota3 + nota4) / divisor;
+
+         System.out.println(media);
+    }
+}
+
+// A média do aluno foi de 8 pontos.
