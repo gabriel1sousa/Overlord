a3192efd93a2f9d97c5a3a1ca5a2598954413c07
gabriel1sousa <105893429+gabriel1sousa@users.noreply.github.com>
2022-08-18 18:38:48 -0300

    Add files via upload
    atividade módulo 9.

---
+++ atvdmod9.java
@@ -0,0 +1,36 @@
+
+
+
+/**
+ *
+ * @author gabriel.sousa1
+ */
+public class atvdmod9 {
+    
+     public static void main(String []args) {
+
+        //Boxing
+        Long cpf = 12847374l;
+
+        Boolean status = false;
+
+        Character c = 'X';
+
+        Integer idade = 18;
+
+        //Unboxing
+        long cpf1 = Long.valueOf(12847374l);
+
+        boolean status2 = Boolean.FALSE;
+
+        char letra = Character.valueOf('X');
+
+        int idade1 = Integer.valueOf(18);
+
+    }
+
+
+}
+
+    
+
