216cf0c8a1b0427388ba3e912f3d7f9b2fd725ea
gabriel1sousa <105893429+gabriel1sousa@users.noreply.github.com>
2022-09-16 19:55:03 -0300

    Add files via upload
    Atividade módulo 11.

---
+++ Modulo11.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.modulo11;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Modulo11 {
+
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner(System.in);
+
+    ArrayList<String> agenda = new ArrayList();
+
+  
+        System.out.println("  agenda.add(\"Maurício Antunes; Masculino\");\n" +
+"    agenda.add(\"Marcos Ribeiro; Masculino\");\n" +
+"    agenda.add(\"Maria Eduarda;Feminino\");\n" +
+"    agenda.add(\"Antônio Fontes;Masculino\");\n" +
+"    ");
+
+        
+    }
+}
