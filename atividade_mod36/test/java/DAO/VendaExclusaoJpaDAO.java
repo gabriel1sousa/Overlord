255c325ae2800894d6f925fe97ac0cecae03239f
gabriel1sousa <105893429+gabriel1sousa@users.noreply.github.com>
2024-06-21 18:08:13 -0300

    Add files via upload

---
+++ atividade_mod36/test/java/DAO/VendaExclusaoJpaDAO.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gabri
+ */
+
+import br.com.gabri.dao.generic.jpa.GenericJpaDB1DAO;
+import br.com.gabri.dao.jpa.IVendaJpaDAO;
+import br.com.gabri.domain.jpa.VendaJpa;
+import br.com.gabri.exceptions.DAOException;
+import br.com.gabri.exceptions.TipoChaveNaoEncontradaException;
+
+public class VendaExclusaoJpaDAO extends GenericJpaDB1DAO<VendaJpa, Long> implements IVendaJpaDAO {
+
+	public VendaExclusaoJpaDAO() {
+		super(VendaJpa.class);
+	}
+
+	@Override
+	public void finalizarVenda(VendaJpa venda) throws TipoChaveNaoEncontradaException, DAOException {
+		throw new UnsupportedOperationException("OPERAÇÃO NÃO PERMITIDA");
+	}
+
+	@Override
+	public void cancelarVenda(VendaJpa venda) throws TipoChaveNaoEncontradaException, DAOException {
+		throw new UnsupportedOperationException("OPERAÇÃO NÃO PERMITIDA");
+	}
+
+	@Override
+	public VendaJpa consultarComCollection(Long id) {
+		throw new UnsupportedOperationException("OPERAÇÃO NÃO PERMITIDA");
+	}
+
+}
