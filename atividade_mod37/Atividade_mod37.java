053b908d149bb67e48165dd6c37d0a68078b6249
gabriel1sousa <105893429+gabriel1sousa@users.noreply.github.com>
2024-06-24 20:18:51 -0300

    Add files via upload

---
+++ atividade_mod37/Atividade_mod37.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade_mod37;
+
+/**
+ *
+ * @author gabri
+ */
+public class Atividade_mod37 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
